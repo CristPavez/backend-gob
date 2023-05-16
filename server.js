fca9ff75ac34ea2bcbd6c80f850b17e8bc5aa6a4
cpavez <cpavez@propulsow.cl>
2023-05-15 23:49:48 -0400

    feat: se cambio para vercel

--- server.js
+++ server.js
@@ -1,22 +1,17 @@
-const express = require("express");
 const AdmZip = require("adm-zip");
 const axios = require("axios");
-const fs = require("fs");
 
-const app = express();
-
-app.get("/api/v1/dataset", async (req, res) => {
+module.exports = async (req, res) => {
   try {
     const zipUrl =
       "https://datos.gob.cl/dataset/5e8bb1f8-f0a5-4719-a877-38543545505b/resource/a7bf9f01-eb74-423a-9f59-5072cb123a14/download/gtfs-v82-po20230114.zip"; // URL del archivo .zip
-    const zipFileName = "archivo.zip"; // Nombre de archivo temporal para guardar el .zip descargado
 
     // Descargar el archivo .zip
     const response = await axios.get(zipUrl, { responseType: "arraybuffer" });
-    fs.writeFileSync(zipFileName, response.data);
+    const zipData = response.data;
 
     // Extraer archivos .txt del .zip descargado
-    const zip = new AdmZip(zipFileName);
+    const zip = new AdmZip(zipData);
     const entries = zip.getEntries();
     const archivosTxt = entries.filter((entry) =>
       entry.entryName.endsWith(".txt")
@@ -31,15 +26,12 @@ app.get("/api/v1/dataset", async (req, res) => {
       };
     });
 
-    // Eliminar el archivo .zip temporal
-    fs.unlinkSync(zipFileName);
-
-    res.json(contenidoJson);
+    res.status(200).json(contenidoJson);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Error al procesar la solicitud" });
   }
-});
+};
 
 function parseDataToJson(data) {
   const lines = data.split("\n");
@@ -56,7 +48,3 @@ function parseDataToJson(data) {
 
   return jsonArray;
 }
-
-app.listen(3000, "0.0.0.0", () => {
-  console.log("Servidor iniciado en el puerto 3000");
-});
