5676fab65200aee590098a70167d0a4feb945fe3
cpavez <cpavez@propulsow.cl>
2023-05-17 10:43:42 -0400

    feat: space

--- server.js
+++ server.js
@@ -8,22 +8,16 @@ const app = express();
 
 app.get("/api/v1/dataset/:nombreArchivo", async (req, res) => {
   try {
-    const cache = {}; // Agregar esta línea para definir la variable cache
+    const cache = {};
     const zipUrl =
-      "https://datos.gob.cl/dataset/5e8bb1f8-f0a5-4719-a877-38543545505b/resource/a7bf9f01-eb74-423a-9f59-5072cb123a14/download/gtfs-v82-po20230114.zip"; // URL del archivo .zip
-    const zipFileName = "archivo.zip"; // Nombre de archivo temporal para guardar el .zip descargado
+      "https://datos.gob.cl/dataset/5e8bb1f8-f0a5-4719-a877-38543545505b/resource/a7bf9f01-eb74-423a-9f59-5072cb123a14/download/gtfs-v82-po20230114.zip";
+    const zipFileName = "archivo.zip";
 
-    // Descargar el archivo .zip
     const response = await axios.get(zipUrl, { responseType: "arraybuffer" });
     fs.writeFileSync(zipFileName, response.data);
 
-    // Extraer archivos .txt del .zip descargado
     const zip = new AdmZip(zipFileName);
     const entries = zip.getEntries();
-    const archivosTxt = entries.filter((entry) =>
-      entry.entryName.endsWith(".txt")
-    );
-
     const archivoTxt = entries.find((entry) =>
       entry.entryName.endsWith(`${req.params.nombreArchivo}.txt`)
     );
@@ -42,27 +36,26 @@ app.get("/api/v1/dataset/:nombreArchivo", async (req, res) => {
 
     const nombreArchivo = req.params.nombreArchivo;
 
-    // Verificar si los datos están en la caché
     if (cache[nombreArchivo]) {
       res.json(cache[nombreArchivo]);
       return;
     }
 
-    // Comprimir el JSON utilizando la biblioteca zlib
     const compressedData = zlib.gzipSync(JSON.stringify(jsonData));
 
-    // Establecer las cabeceras de respuesta
     res.set({
       "Content-Type": "application/json",
       "Content-Encoding": "zip",
       "Content-Disposition": `attachment; filename=${nombreArchivo}.zip`,
     });
 
-    // Enviar el JSON comprimido como respuesta
     res.send(compressedData);
+
+    // Eliminar el archivo zip temporal después de enviar la respuesta
+    fs.unlinkSync(zipFileName);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: error.message }); // Mostrar el mensaje de error específico
+    res.status(500).json({ error: error.message });
   }
 });
 
@@ -74,7 +67,6 @@ function parseDataToJson(data) {
     const values = line.split(",");
     const jsonObject = {};
 
-    // Verificar si la línea está vacía
     if (values.some((value) => value.trim().length > 0)) {
       headers.forEach((header, index) => {
         const key = header.trim();
