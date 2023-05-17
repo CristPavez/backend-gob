f8b27153e26ccbff6b5cb7d139789e33e1679023
Cristian Pavez <cristian.pavez.medina@gmail.com>
2023-05-16 22:44:28 -0400

    Update server.js a

--- server.js
+++ server.js
@@ -1,13 +1,36 @@
+const express = require("express");
+const AdmZip = require("adm-zip");
+const axios = require("axios");
+const fs = require("fs");
 
-//a
-module.exports = async (req, res) => {
+const app = express();
+
+app.get("/api/v1/dataset", async (req, res) => {
   try {
  
-    res.json("xd");
-  }  catch (error) {
+    res.json('0');
+  } catch (error) {
     console.error(error);
-    res.status(500).json({ error: error.message }); // Mostrar el mensaje de error específico
+    res.status(500).json({ error: "Error al procesar la solicitud" });
   }
+});
 
-};
- 
+function parseDataToJson(data) {
+  const lines = data.split("\n");
+  const headers = lines[0].split(",");
+
+  const jsonArray = lines.slice(1).map((line) => {
+    const values = line.split(",");
+    const jsonObject = {};
+    headers.forEach((header, index) => {
+      jsonObject[header] = values[index];
+    });
+    return jsonObject;
+  });
+
+  return jsonArray;
+}
+
+app.listen(3000, "0.0.0.0", () => {
+  console.log("Servidor iniciado en el puerto 3000");
+});
