4b72a36723a532d8e88d673d4efecee14326bb0a
Cristian Pavez <cristian.pavez.medina@gmail.com>
2023-05-16 22:23:17 -0400

    Update server.js V

--- server.js
+++ server.js
@@ -6,42 +6,53 @@ module.exports = async (req, res) => {
   try {
     const zipUrl =
       "https://datos.gob.cl/dataset/5e8bb1f8-f0a5-4719-a877-38543545505b/resource/a7bf9f01-eb74-423a-9f59-5072cb123a14/download/gtfs-v82-po20230114.zip"; // URL del archivo .zip
+    const zipFileName = "archivo.zip"; // Nombre de archivo temporal para guardar el .zip descargado
 
     // Descargar el archivo .zip
     const response = await axios.get(zipUrl, { responseType: "arraybuffer" });
-    const zipData = response.data;
+    fs.writeFileSync(zipFileName, response.data);
 
     // Extraer archivos .txt del .zip descargado
-    const zip = new AdmZip(zipData);
+    const zip = new AdmZip(zipFileName);
     const entries = zip.getEntries();
     const archivosTxt = entries.filter((entry) =>
       entry.entryName.endsWith(".txt")
     );
 
-    const jsonResponses = [];
+    const data = zip.readAsText(archivosTxt[0]);
+    const jsonData = parseDataToJson(data); // Función para transformar el contenido de texto a JSON
+    console.log(jsonData);
+    const compressedData = compressJSON(jsonData); // Función para comprimir el JSON
 
-    // Transformar cada archivo .txt a JSON
-    for (const archivoTxt of archivosTxt) {
-      const txtData = zip.readAsText(archivoTxt);
-      const jsonData = transformToJSON(txtData);
-      const compressedData = compressJSON(jsonData);
-      jsonResponses.push(compressedData);
-    }
+    // Descomprimir el contenido comprimido
+    const uncompressedData = zlib.unzipSync(compressedData);
 
-    return res.status(200).json(jsonResponses);
+    // Eliminar el archivo .zip temporal
+    fs.unlinkSync(zipFileName);
+
+    res.json(JSON.parse(uncompressedData));
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ error: "Error al procesar la solicitud." });
+    res.status(500).json({ error: "Error al procesar la solicitud" });
   }
-};
+});
 
-function transformToJSON(txtData) {
-  // Lógica para transformar el contenido del archivo .txt a JSON
-  // Puedes usar JSON.parse() u otras técnicas dependiendo de la estructura de tus archivos .txt
+function parseDataToJson(data) {
+  const lines = data.split("\n");
+  const headers = lines[0].split(",");
 
-  const jsonData = JSON.parse(txtData); // Reemplaza esto con la transformación real
+  const jsonArray = lines.slice(1).map((line) => {
+    const values = line.split(",");
+    const jsonObject = {};
+    headers.forEach((header, index) => {
+      const key = header.trim(); // Eliminar espacios en blanco alrededor de la clave
+      const value = values[index]?.trim(); // Eliminar espacios en blanco alrededor del valor
+      jsonObject[key] = value?.replace(/\r$/, ""); // Eliminar el carácter de retorno de carro (\r)
+    });
+    return jsonObject;
+  });
 
-  return jsonData;
+  return jsonArray;
 }
 
 function compressJSON(jsonData) {
