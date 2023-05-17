71554cb5778b358217ec9a55d1aca66a70037f69
cpavez <cpavez@propulsow.cl>
2023-05-17 11:46:47 -0400

    feat: me dolio esto commit :C

--- server.js
+++ server.js
@@ -8,16 +8,22 @@ const app = express();
 
 app.get("/api/v1/dataset/:nombreArchivo", async (req, res) => {
   try {
-    const cache = {};
+    const cache = {}; // Agregar esta línea para definir la variable cache
     const zipUrl =
-      "https://datos.gob.cl/dataset/5e8bb1f8-f0a5-4719-a877-38543545505b/resource/a7bf9f01-eb74-423a-9f59-5072cb123a14/download/gtfs-v82-po20230114.zip";
-    const zipFileName = "archivo.zip";
+      "https://datos.gob.cl/dataset/5e8bb1f8-f0a5-4719-a877-38543545505b/resource/a7bf9f01-eb74-423a-9f59-5072cb123a14/download/gtfs-v82-po20230114.zip"; // URL del archivo .zip
+    const zipFileName = "archivo.zip"; // Nombre de archivo temporal para guardar el .zip descargado
 
+    // Descargar el archivo .zip
     const response = await axios.get(zipUrl, { responseType: "arraybuffer" });
     fs.writeFileSync(zipFileName, response.data);
 
+    // Extraer archivos .txt del .zip descargado
     const zip = new AdmZip(zipFileName);
     const entries = zip.getEntries();
+    const archivosTxt = entries.filter((entry) =>
+      entry.entryName.endsWith(".txt")
+    );
+
     const archivoTxt = entries.find((entry) =>
       entry.entryName.endsWith(`${req.params.nombreArchivo}.txt`)
     );
@@ -28,34 +34,27 @@ app.get("/api/v1/dataset/:nombreArchivo", async (req, res) => {
     }
 
     const data = zip.readAsText(archivoTxt);
-
     const jsonData = {
-      nombre: archivoTxt.name.replace(/\.txt$/, ""),
-      contenido: parseDataToJson(data),
+      nombre: archivoTxt.name.replace(/\.txt$/, ""), // Asignar el nombre del archivo al objeto jsonData
+      contenido: parseDataToJson(data), // Función para transformar el contenido de texto a JSON
     };
-
     const nombreArchivo = req.params.nombreArchivo;
-
+    // Verificar si los datos están en la caché
     if (cache[nombreArchivo]) {
       res.json(cache[nombreArchivo]);
       return;
     }
+    // Eliminar el archivo .zip temporal
+    setTimeout(() => {
+      fs.unlinkSync(zipFileName); // Eliminar el archivo .zip temporal después de un cierto período de tiempo
+    }, 60000); // Eliminar después de 1 minuto (ajusta el valor según tus necesidades)
+    // Almacenar los datos en la caché
+    cache[nombreArchivo] = jsonData;
 
-    const compressedData = zlib.gzipSync(JSON.stringify(jsonData));
-
-    res.set({
-      "Content-Type": "application/json",
-      "Content-Encoding": "zip",
-      "Content-Disposition": `attachment; filename=${nombreArchivo}.zip`,
-    });
-
-    res.send(compressedData);
-
-    // Eliminar el archivo zip temporal después de enviar la respuesta
-    fs.unlinkSync(zipFileName);
+    res.json(jsonData);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: error.message });
+    res.status(500).json({ error: error.message }); // Mostrar el mensaje de error específico
   }
 });
 
@@ -67,6 +66,7 @@ function parseDataToJson(data) {
     const values = line.split(",");
     const jsonObject = {};
 
+    // Verificar si la línea está vacía
     if (values.some((value) => value.trim().length > 0)) {
       headers.forEach((header, index) => {
         const key = header.trim();
@@ -82,6 +82,13 @@ function parseDataToJson(data) {
 
   return jsonArray;
 }
+
+// function compressJSON(jsonData) {
+//   // Comprimir el JSON utilizando la biblioteca zlib
+//   const compressedData = zlib.gzipSync(JSON.stringify(jsonData));
+
+//   return compressedData;
+// }
 
 app.listen(3000, "0.0.0.0", () => {
   console.log("Servidor iniciado en el puerto 3000");
